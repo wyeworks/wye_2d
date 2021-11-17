e781b9151e77216204ef7697a11073c0d9956477
Julián Montes de Oca <julian@wyeworks.com>
2021-11-17 08:12:20 -0300

    Add base structure of the game state (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,49 @@
 use ggez::*;
+pub mod entities {
+    pub mod desk;
+    pub mod map;
+    pub mod player;
+}
 
+use entities::map::*;
+
+// Position of the elements in the screen, this will be used by all entities
+pub struct Position {
+    x: f32,
+    y: f32,
+}
+
+// The main game state
 struct State {
-    dt: std::time::Duration,
+    map: Map,
+}
+
+impl State {
+    pub fn new() -> Self {
+        State { map: Map::new() }
+    }
 }
 
 impl ggez::event::EventHandler<GameError> for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        self.dt = timer::delta(ctx);
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        println!("Hello ggez! dt = {}ns", self.dt.as_nanos());
+        self.map.draw(ctx)?;
         Ok(())
     }
 }
 
-pub fn main() {
-    let state = State {
-        dt: std::time::Duration::new(0, 0),
-    };
-
+fn main() -> GameResult {
     let c = conf::Conf::new();
-    let (ctx, event_loop) = ContextBuilder::new("hello_ggez", "awesome_person")
+
+    let (ctx, event_loop) = ContextBuilder::new("wye_2D", "rust_team")
         .default_conf(c)
         .build()
         .unwrap();
 
+    graphics::set_window_title(&ctx, "Welcome to Wyeworks!");
+
+    let state = State::new();
     event::run(ctx, event_loop, state);
 }
