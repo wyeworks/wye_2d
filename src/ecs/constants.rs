f149a404daaaf20b26c0c941f23421fe79344a09
Julian <julian@wyeworks.com>
2021-12-23 12:43:47 -0300

    Remove unnecessary comments

--- src/ecs/constants.rs
+++ src/ecs/constants.rs
@@ -1,5 +1,3 @@
-//use ggez::graphics;
-
 use super::systems::physics_system::positioning::positioning::Position;
 
 pub const HUMANOID_W: f32 = 25.0;
@@ -9,6 +7,4 @@ pub const DESK_W: f32 = 150.0;
 
 pub const INITIAL_PLAYER_POS: Position = Position { x: 500.0, y: 600.0 };
 pub const INITIAL_PLAYER_SPEED: f32 = 125.0;
-//pub const INITIAL_PLAYER_COLOR: graphics::Color = graphics::Color::from_rgb(0, 171, 169);
 
-//pub const INITIAL_WW_COLOR: graphics::Color = graphics::Color::from_rgb(112, 111, 211);
